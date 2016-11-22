902d520a02b639c1bf8f7c496f7de71b297ba0b8
Jalen <jalen@kuzcolighting.com>
2016-11-22 12:02:04 -0800

    Move some business logic from ChatController to ChatService

--- src/main/java/com/itdoes/csm/service/ChatService.java
+++ src/main/java/com/itdoes/csm/service/ChatService.java
@@ -1,5 +1,7 @@
 package com.itdoes.csm.service;
 
+import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -9,11 +11,13 @@ import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Lists;
@@ -25,6 +29,8 @@ import com.itdoes.common.business.service.EntityDbService;
 import com.itdoes.common.core.jpa.FindFilter;
 import com.itdoes.common.core.jpa.FindFilter.Operator;
 import com.itdoes.common.core.jpa.Specifications;
+import com.itdoes.common.core.shiro.ShiroUser;
+import com.itdoes.common.core.shiro.Shiros;
 import com.itdoes.common.core.util.Collections3;
 import com.itdoes.csm.dto.ChatEvent;
 import com.itdoes.csm.dto.ChatUser;
@@ -71,7 +77,43 @@ public class ChatService extends BaseService {
 		messagePair = env.getPair(CsmChatMessage.class.getSimpleName());
 	}
 
-	public List<ChatUser> getCustomerList() {
+	public List<CsmChatMessage> customerInitMessage(Principal principal) {
+		final ShiroUser shiroUser = Shiros.getShiroUser(principal);
+		final String userId = shiroUser.getId();
+		List<CsmChatMessage> messageList = adminInitMessage(userId);
+		if (Collections3.isEmpty(messageList)) {
+			messageList = Lists.newArrayListWithCapacity(1);
+		}
+
+		final CsmChatMessage message = new CsmChatMessage();
+		message.setSenderName("Customer Service");
+		message.setCreateDateTime(LocalDateTime.now());
+		message.setMessage("Welcome, " + shiroUser.getUsername() + "! Our agent will contact you soon. Please wait...");
+		message.setFromAdmin(true);
+		messageList.add(message);
+
+		return messageList;
+	}
+
+	public void customerSendMessage(CsmChatMessage message, Principal principal, SimpMessagingTemplate template) {
+		final ShiroUser shiroUser = Shiros.getShiroUser(principal);
+		final String userId = shiroUser.getId();
+		final UUID userIdUuid = UUID.fromString(userId);
+		message.setRoomId(userIdUuid);
+		message.setSenderId(userIdUuid);
+		message.setSenderName(shiroUser.getUsername());
+		message.setCreateDateTime(LocalDateTime.now());
+		message.setFromAdmin(false);
+		template.convertAndSend("/topic/chat/message/" + userId, message);
+
+		final ChatEvent messageEvent = new ChatEvent(userId);
+		template.convertAndSend("/topic/chat/addUnhandledCustomer", messageEvent);
+		addUnhandledCustomer(messageEvent);
+
+		saveChatMessage(message);
+	}
+
+	public List<ChatUser> adminInit() {
 		final Set<String> customerIdSet = userCacheService.getCustomerIdSet();
 		final List<ChatUser> customerList = Lists.newArrayListWithCapacity(customerIdSet.size());
 		for (String customerId : customerIdSet) {
@@ -84,23 +126,7 @@ public class ChatService extends BaseService {
 		return customerList;
 	}
 
-	public void addUnhandledCustomer(ChatEvent event) {
-		unhandledCustomerMap.put(event.getUserId(), event);
-	}
-
-	public void removeUnhandledCustomer(String userId) {
-		unhandledCustomerMap.remove(userId);
-	}
-
-	public boolean hasUnhandledCustomer() {
-		return !Collections3.isEmpty(unhandledCustomerMap);
-	}
-
-	public void saveChatMessage(CsmChatMessage message) {
-		dbService.save(messagePair, message);
-	}
-
-	public List<CsmChatMessage> getChatMessageList(String roomId) {
+	public List<CsmChatMessage> adminInitMessage(String roomId) {
 		final List<CsmChatMessage> dbMessageList = getChatMessageListFromDb(roomId);
 		if (Collections3.isEmpty(dbMessageList)) {
 			return Collections.emptyList();
@@ -121,6 +147,42 @@ public class ChatService extends BaseService {
 			messageList.add(message);
 		}
 		return messageList;
+	}
+
+	public void adminSendMessage(CsmChatMessage message, Principal principal, SimpMessagingTemplate template) {
+		final UUID roomIdUuid = message.getRoomId();
+		Validate.notNull(roomIdUuid, "RoomId should not be null");
+
+		final ShiroUser shiroUser = Shiros.getShiroUser(principal);
+		final String userId = shiroUser.getId();
+		final UUID userIdUuid = UUID.fromString(userId);
+		message.setSenderId(userIdUuid);
+		message.setSenderName(shiroUser.getUsername());
+		message.setCreateDateTime(LocalDateTime.now());
+		message.setFromAdmin(true);
+		template.convertAndSend("/topic/chat/message/" + roomIdUuid, message);
+
+		final ChatEvent messageEvent = new ChatEvent(roomIdUuid.toString());
+		template.convertAndSend("/topic/chat/removeUnhandledCustomer", messageEvent);
+		removeUnhandledCustomer(messageEvent.getUserId());
+
+		saveChatMessage(message);
+	}
+
+	public boolean hasUnhandledCustomer() {
+		return !Collections3.isEmpty(unhandledCustomerMap);
+	}
+
+	private void addUnhandledCustomer(ChatEvent event) {
+		unhandledCustomerMap.put(event.getUserId(), event);
+	}
+
+	private void removeUnhandledCustomer(String userId) {
+		unhandledCustomerMap.remove(userId);
+	}
+
+	private void saveChatMessage(CsmChatMessage message) {
+		dbService.save(messagePair, message);
 	}
 
 	private List<CsmChatMessage> getChatMessageListFromDb(String roomId) {
