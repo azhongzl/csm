7af29a226da4dbd8944cf1329a6467c77462109c
Jalen <jalen@kuzcolighting.com>
2016-11-23 13:23:26 -0800

    Enhance CsmUserGroupDao to fetch all the permissions from given user group (including sub user groups)

--- src/main/java/com/itdoes/csm/dao/CsmUserGroupDao.java
+++ src/main/java/com/itdoes/csm/dao/CsmUserGroupDao.java
@@ -8,9 +8,13 @@ import com.itdoes.csm.entity.CsmUserGroup;
  * @author Jalen Zhong
  */
 public interface CsmUserGroupDao extends com.itdoes.common.business.dao.BaseDao<CsmUserGroup, java.util.UUID> {
-	@org.springframework.data.jpa.repository.Query(value = "select p.permission"
-			+ " from csm_user_group ug, csm_user_group_role ugr, csm_role r, csm_role_permission rp, csm_permission p"
-			+ " where ug.id = ugr.user_group_id and ugr.role_id = r.id and rp.role_id = r.id and rp.permission_id = p.id and ug.id = :userGroupId", nativeQuery = true)
-	java.util.Set<String> findPermissionById(
-			@org.springframework.data.repository.query.Param("userGroupId") java.util.UUID userGroupId);
+	@org.springframework.data.jpa.repository.Query(value = "SELECT p.permission"
+			+ " FROM csm_user_group ug, csm_user_group_role ugr, csm_role r, csm_role_permission rp, csm_permission p"
+			+ " WHERE ugr.user_group_id = ug.id AND ugr.role_id = r.id AND rp.role_id = r.id AND rp.permission_id = p.id"
+			+ " AND (" + " ug.id = ?1" + " OR FIND_IN_SET(ug.id," + " (SELECT GROUP_CONCAT(Level SEPARATOR ',') FROM ("
+			+ " SELECT @Ids \\:= (" + " SELECT GROUP_CONCAT(id SEPARATOR ',')" + " FROM csm_user_group"
+			+ " WHERE FIND_IN_SET(super_id, @Ids)" + " ) Level" + " FROM csm_user_group"
+			+ " JOIN (SELECT @Ids \\:= ?1) temp1" + " WHERE FIND_IN_SET(super_id, @Ids)" + " ) temp2)"
+			+ " ))", nativeQuery = true)
+	java.util.Set<String> findPermission(java.util.UUID userGroupId);
 }
