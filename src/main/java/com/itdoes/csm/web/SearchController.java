a00f337ede23b2cbdbd2e13a5d9fb8b29f34fd2e
Jalen <jalen@kuzcolighting.com>
2016-11-24 12:37:31 -0800

    Replace CustomSearchController with SearchController

--- src/main/java/com/itdoes/csm/web/SearchController.java
+++ src/main/java/com/itdoes/csm/web/SearchController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.itdoes.common.business.service.SearchService;
 import com.itdoes.common.business.web.BaseController;
-import com.itdoes.common.business.web.SearchController;
 import com.itdoes.common.core.Result;
 import com.itdoes.common.core.web.HttpResults;
 import com.itdoes.common.core.web.MediaTypes;
@@ -21,16 +20,19 @@ import com.itdoes.csm.entity.CsmFaq;
  * @author Jalen Zhong
  */
 @RestController
-@RequestMapping(value = SearchController.SEARCH_URL_PREFIX, produces = MediaTypes.APPLICATION_JSON_UTF_8)
-public class CustomSearchController extends BaseController {
-	public static final String SEARCH_COMMAND_FAQ = "CsmFaq";
-
+public class SearchController extends BaseController {
 	private static final String[] FAQ_FIELDS = { "question", "answer" };
 
 	@Autowired
 	private SearchService searchService;
 
-	@RequestMapping(value = "/" + CustomSearchController.SEARCH_COMMAND_FAQ, method = RequestMethod.GET)
+	@RequestMapping(value = "/admin/search/createIndex", method = RequestMethod.GET, produces = MediaTypes.APPLICATION_JSON_UTF_8)
+	public Result createIndex() {
+		searchService.createIndex();
+		return HttpResults.success();
+	}
+
+	@RequestMapping(value = "/search/CsmFaq", method = RequestMethod.GET, produces = MediaTypes.APPLICATION_JSON_UTF_8)
 	public Result searchFaq(@RequestParam(value = "ss") String searchString,
 			@RequestParam(value = "page_no", defaultValue = "1") int pageNo,
 			@RequestParam(value = "page_size", defaultValue = "-1") int pageSize,
