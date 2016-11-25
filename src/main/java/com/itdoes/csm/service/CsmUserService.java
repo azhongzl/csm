fb4aeb5dc5952588a2c780cc0936fbb4cd53d527
Jalen <jalen@kuzcolighting.com>
2016-11-25 00:50:30 -0800

    Add an empty CsmUserService

---
+++ src/main/java/com/itdoes/csm/service/CsmUserService.java
@@ -0,0 +1,13 @@
+package com.itdoes.csm.service;
+
+import org.springframework.stereotype.Service;
+
+import com.itdoes.common.business.service.EntityService;
+
+/**
+ * @author Jalen Zhong
+ */
+@Service
+public class CsmUserService extends EntityService {
+
+}
