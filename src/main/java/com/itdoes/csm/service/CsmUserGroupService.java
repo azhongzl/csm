125a4b1693d8ee687e245693222d32cd35cfdee5
Jalen <jalen@kuzcolighting.com>
2016-11-28 21:05:47 -0800

    Add CsmUserGroupService

---
+++ src/main/java/com/itdoes/csm/service/CsmUserGroupService.java
@@ -0,0 +1,45 @@
+package com.itdoes.csm.service;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itdoes.common.business.EntityPair;
+import com.itdoes.common.business.service.EntityService;
+import com.itdoes.csm.entity.CsmUserGroup;
+
+/**
+ * @author Jalen Zhong
+ */
+public class CsmUserGroupService extends EntityService {
+	@Autowired
+	private UserCacheService userCacheService;
+
+	@Override
+	public <T, ID extends Serializable> ID post(EntityPair<T, ID> pair, T entity) {
+		final CsmUserGroup userGroup = (CsmUserGroup) entity;
+
+		final ID id = super.post(pair, entity);
+
+		userCacheService.addUserGroup(userGroup);
+
+		return id;
+	}
+
+	@Override
+	public <T, ID extends Serializable> void put(EntityPair<T, ID> pair, T entity, T oldEntity) {
+		final CsmUserGroup userGroup = (CsmUserGroup) entity;
+
+		super.put(pair, entity, oldEntity);
+
+		userCacheService.updateUserGroup(userGroup);
+	}
+
+	@Override
+	public <T, ID extends Serializable> void delete(EntityPair<T, ID> pair, ID id, String realRootPath,
+			boolean uploadDeleteOrphanFiles) {
+		super.delete(pair, id, realRootPath, uploadDeleteOrphanFiles);
+
+		userCacheService.removeUserGroup(id.toString());
+	}
+}
