9db32093d505b2b27992153533287d68d2262c3e
Jalen <jalen@kuzcolighting.com>
2016-12-05 21:06:50 -0800

    Adapt to EntityExternalService change in common module

--- src/main/java/com/itdoes/csm/service/entity/external/CsmUserExternalService.java
+++ src/main/java/com/itdoes/csm/service/entity/external/CsmUserExternalService.java
@@ -8,9 +8,7 @@ import org.apache.shiro.subject.Subject;
 import com.itdoes.common.business.EntityPair;
 import com.itdoes.common.business.Perms;
 import com.itdoes.common.business.entity.EntityPermCommand;
-import com.itdoes.common.business.service.entity.external.EntityExternalPermFieldService;
 import com.itdoes.common.business.service.entity.external.EntityExternalService;
-import com.itdoes.common.business.service.entity.internal.EntityInternalService;
 import com.itdoes.common.core.shiro.ShiroUser;
 import com.itdoes.common.core.util.Reflections;
 import com.itdoes.csm.codegenerator.entity.entity.CsmUser;
@@ -19,11 +17,6 @@ import com.itdoes.csm.codegenerator.entity.entity.CsmUser;
  * @author Jalen Zhong
  */
 public class CsmUserExternalService extends EntityExternalService {
-	public CsmUserExternalService(EntityInternalService internalService,
-			EntityExternalPermFieldService permFieldService) {
-		super(internalService, permFieldService);
-	}
-
 	@Override
 	public <T, ID extends Serializable> T get(EntityPair<T, ID> pair, ID id) {
 		boolean permitted = false;
